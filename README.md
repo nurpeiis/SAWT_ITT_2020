a528a4d1f67f03dc1aafb2342ba882892b8b76d2
Nurpeiis Baimukan <nb2577@nyu.edu>
2020-02-02 21:24:34 +0400

    Include All Main Functions for ASR

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ We propose two solutions for people who are faced with Speech Pathologies as a d
 
 1. Automatic Speech Recognizer and Speech Generator
 2. Smart matching of Speech-Language Pathologists based on your disease background, location and etc.
+
+## Requirements
+
+- Python 3.6
+- FFMPEG package
+- DeepSpeech 0.6.1
--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
 
-from telegram import Updater, CommandHandle
+import subprocess
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 
-def start(update, source):
+AUDIO = range(1)
+
+def start(update, context):
     """ 
         Welcome Message
     """
+    update.message.reply('Welcome to SAWT bot! \n\n I am Automatic Speech Recogniser! \n\n Press Audio recording Button and record a sentence in English')
+
+    return AUDIO
+
 
 def convert(src_file, dst_file):
     """ 
@@ -21,3 +29,33 @@ def convert(src_file, dst_file):
             p.wait()
             return False
     return True
+
+def analyze(update, context):
+    """ 
+        Given Audio File it will perform S2T using DeepSpeech
+    """
+    pass
+
+def cancel(update, context):
+    update.message.reply_text('GoodBye!!!')
+
+def main():
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            AUDIO: [MessageHandler(Filters.audio, analyze)],
+        },
+        allow_reentry=True,
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+
+if __name__ == '__main__':
+    main()
